e847872b90faff42c8cbc2f01a8c593eab5df0ce
AC1 <adamcoate1@gmail.com>
2016-08-02 19:48:06 -0400

    adding first test for prime factors kata

---
+++ PrimeFactorsKata/src/main/java/com/swcguild/primefactorskata/PrimeFactors5.java
@@ -0,0 +1,12 @@
+
+package com.swcguild.primefactorskata;
+
+import java.util.List;
+
+public class PrimeFactors5 {
+
+    public static List<Integer> generate(int num) {
+        return null;
+    }
+    
+}
---
+++ PrimeFactorsKata/src/test/java/com/swcguild/primefactorskata/PrimeFactors5Test.java
@@ -0,0 +1,29 @@
+
+package com.swcguild.primefactorskata;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class PrimeFactors5Test {
+    
+    private PrimeFactors5 prime;
+    
+    @Before
+    public void setUp() {
+        prime = new PrimeFactors5();
+    }
+    
+    @Test
+    public void one() {
+        assertEquals(list(), prime.generate(1));
+    }
+    
+    private List<Integer> list() {
+        return new ArrayList<>();
+    }
+    
+}
